b6fdc70e11fc9788ffee0479b23ebef3aab7dcc1
YannCombette <combette.yann@oclock.school>
2023-07-20 17:22:13 -0400

    create class folder with Season class created

---
+++ classes/Seasons.php
@@ -0,0 +1,104 @@
+<?php
+
+class Seasons
+{
+    private $season;
+    private $days;
+    private $date;
+    private $author;
+    private $category;
+
+    public function __construct($season = '', $days = '', $author = '', $date = '', $category = '')
+    {
+        $this->season = $season;
+        $this->days = $days;
+        $this->author = $author;
+        $this->date = $date;
+        $this->category = $category;
+    }
+
+    public function getDateFr()
+    {
+        return date('d/m/Y', strtotime($this->date));
+    }
+
+    /**
+     * Get the value of season
+     */ 
+    public function getSeason()
+    {
+        return $this->season;
+    }
+
+    /**
+     * Set the value of season
+     *
+     * @return  self
+     */ 
+    public function setSeason($season)
+    {
+        $this->season = $season;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of days
+     */ 
+    public function getSays()
+    {
+        return $this->days;
+    }
+
+    /**
+     * Set the value of days
+     *
+     * @return  self
+     */ 
+    public function setDays($days)
+    {
+        $this->days = $days;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of author
+     */ 
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set the value of author
+     *
+     * @return  self
+     */ 
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of category
+     */ 
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * Set the value of category
+     *
+     * @return  self
+     */ 
+    public function setCategory($category)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
