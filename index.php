f1033effe581929c7a024de5b0adf06192510643
YannCombette <combette.yann@oclock.school>
2023-07-20 15:53:01 -0400

    moved header homepage.tpl.php-> header.tpl.php and repuire it in index

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php 
 
+require __DIR__ . '/templates/header.tpl.php';
 require __DIR__ . '/templates/home.tpl.php';
---
+++ templates/header.tpl.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Localhost</title>
+
+    <!-- On va chercher bootstrap via leur CDN -->
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+</head>
+
+<body>
+    <!-- On utilise le composant navbar de la doc Bootstrap -->
+    <!-- https://getbootstrap.com/docs/5.0/components/navbar/ -->
+    <nav class="navbar navbar-expand-lg navbar-expand-md navbar-light bg-light">
+        <div class="container-fluid">
